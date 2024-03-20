44902afc512a0accec91de9f4fd13083709bc7d8
ava.luxe <ajrluxe@gmail.com>
2024-03-19 22:45:08 -0500

    completion of index import for Boards component, failure to npm run build

--- react-vite/src/components/Boards/index.js
+++ react-vite/src/components/Boards/index.js
@@ -1,5 +1,11 @@
 import AllBoards from "./AllBoards/AllBoards";
 import SingleBoard from "./SingleBoard/SingleBoard";
 import PostBoard from "./PostBoards/PostBoard";
+import EditBoard from "./EditBoards/EditBoard";
+import DeleteBoard from "./AllBoards/DeleteBoards/DeleteBoard";
 
-export { AllBoards, SingleBoard, PostBoard };
+
+
+
+
+export { AllBoards, SingleBoard, PostBoard, EditBoard, DeleteBoard };
