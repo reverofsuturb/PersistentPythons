191397528a58cb32df4d639321c4e1b757851802
Stryker Huber <stryker.important@gmail.com>
2024-03-20 23:49:20 -0700

    first chagnes

--- react-vite/src/components/MainPage/MainPage.jsx
+++ react-vite/src/components/MainPage/MainPage.jsx
@@ -1,13 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import groupImage from '../../images/mainpage/groups.jpg'
-import eventImage from '../../images/mainpage/events.jpg'
-import createGroupImage from '../../images/mainpage/createGroup.jpg'
-import mainImage from '../../images/mainpage/mainpagePhoto.jpg'
 import './MainPage.css';
-import NotLoggedInModal from '../PermissionModal/PermissionModal';
 import { useModal } from '../../context/Modal';
 import SignupFormModal from '../SignupFormModal';
+
+import mainPhoto from '../../images/MainPage_oto.png'
 
 export default function MainPage() {
     const sessionUser = useSelector((state) => state.session.user);
@@ -34,42 +31,48 @@ export default function MainPage() {
 
     return (
         <div className="main-page">
+            <div className='tagContainer'>
+                <div className='tag_bar'>
+                    <h4>Accelerate your teams travel plans with Trans-galactic Artificial Intelligence Facilitating Orbital Organic Distance (TAI FOOD)</h4>
+                </div>
+            </div>
 
             <div className='welcome'>
                 <div className='welcomeText'>
-                    <h1 className='fixtxtalign' >The Introverts Platform</h1>
-                    <h2 className='fixtxtalign' >Where distance becomes friendships</h2>
-                    <p id='ptagchagne' >“The Introverts Platform” is a revolutionary digital space designed specifically for introverts. It provides a safe, comfortable environment where introverts can connect, share ideas, and build meaningful relationships while respecting their need for personal space and solitude.</p>
-                    <p id='ptagchagne' >“The platform offers a variety of features tailored to the introvert lifestyle. For instance, users can engage in one-on-one conversations or participate in small group discussions, all at their own pace. The platform also includes a ‘Quiet Mode’, allowing users to take a break from social interactions whenever they need it!</p>
+                    <h1 className='fixtxtalign' >Space Odyssey brings the joy of travel, with the greatness of planning!</h1>
+                    <p id='ptagchagne'>A space odyssey with a date, probably!</p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
                 </div>
                 <div>
-                    <img className='maingImageMainPage' src={mainImage} />
+                    <p></p>
+                    <img className='maingImageMainPage' src={mainPhoto} />
+                    <p></p>
                 </div>
             </div>
 
             {/* I need a center text on how the webpage works and have it centered on the page */}
-            <div className="descriptionmeetup">
-                <h2>How MeetUpThatWay works</h2>
-                <p>You can people watch or just stay at home!</p>
-            </div>
 
 
-            <div style={{cursor:'pointer'}} className="how-it-works">
-                <div className="findgroups" onClick={allGroups} >
-                    <img src={groupImage} alt="See all groups" />
-                    <p>Find all groups away from me!</p>
+
+            <div>
+                <div>
+                    <span>SPACE ODYSSEY 101</span>
+                    <span>A Productive Vaca</span>
+                    <span>Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who’s doing what and what needs to get done. Learn more in our guide for getting started.</span>
                 </div>
 
-                <div className="findevents" onClick={allEvents}>
-                    <img src={eventImage} alt="Find an event" />
-                    <p>Find an event that way!</p>
-                </div>
+                <div>
 
-                <div className="creategroup"
-                    style={!sessionUser ? { pointerEvents: "none", opacity: "0.4" } : { color: 'initial' }}
-                    onClick={createGroup}>
-                    <img src={createGroupImage} alt="Start a new group" />
-                    <p>Start a new introvert binge</p>
                 </div>
 
             </div>
