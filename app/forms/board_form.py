5c96d5c24443d612fd764cd8a30b5f39dca90ce9
Veronica Moss <vivettemoss@yahoo.com>
2024-03-21 21:37:01 -0500

    addedbackendvalidation

--- app/forms/board_form.py
+++ app/forms/board_form.py
@@ -3,4 +3,4 @@ from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 class BoardForm(FlaskForm):
-    board_name = StringField('Board Name', validators=[DataRequired(message="Board name is required"), Length(minx=5 , max=50, message="Board name cannot be less than 5, exceed 50 characters")])
+    board_name = StringField('Board Name', validators=[DataRequired(message="Board name is required"), Length(min=5 , max=50, message="Board name cannot be less than 5, exceed 50 characters")])
