8120e61c7cdfa8cd85a04b0ce84376230c2aa477
ava.luxe <ajrluxe@gmail.com>
2024-03-21 21:13:39 -0500

    added backend validation for boardname

--- app/forms/board_form.py
+++ app/forms/board_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class BoardForm(FlaskForm):
-    board_name = StringField('Board Name', validators=[DataRequired()])
+    board_name = StringField('Board Name', validators=[DataRequired(message="Board name is required"), Length(max=50, message="Board name cannot exceed 50 characters")])
